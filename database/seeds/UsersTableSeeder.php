040a2daa4eadc6a3d2c5b133125490ce08fbe359
高橋広昇 <>
2022-11-19 11:10:23 +0900

    シードを使った登録実装完了

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Models\Users\User;
+use Illuminate\Support\Facades\DB;
 
 class UsersTableSeeder extends Seeder
 {
@@ -12,6 +13,18 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-
+        DB::table('users')->insert([
+            [
+                'over_name' => "高橋",
+                'under_name' =>"広昇",
+                'over_name_kana' => "タカハシ",
+                'under_name_kana' => "ヒロノリ",
+                'mail_address' => "hiro.com",
+                'password' => bcrypt("1234"),
+                'sex' => "1",
+                'birth_day' => "1998-02-11",
+                'role' => "1",
+            ]   
+        ]);
     }
 }
