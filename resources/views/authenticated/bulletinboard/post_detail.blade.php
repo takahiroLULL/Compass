76462f275f44059916c28e2ba38ad6e81218f0f8
高橋広昇 <>
2022-12-24 15:32:26 +0900

    投稿の編集/削除時に確認メッセージを追加　実装完了

--- resources/views/authenticated/bulletinboard/post_detail.blade.php
+++ resources/views/authenticated/bulletinboard/post_detail.blade.php
@@ -7,10 +7,12 @@
         <div class="detail_inner_head">
           <div>
           </div>
+          @if($post->user_id == Auth::id())
           <div>
             <span class="edit-modal-open" post_title="{{ $post->post_title }}" post_body="{{ $post->post }}" post_id="{{ $post->id }}">編集</span>
-            <a href="{{ route('post.delete', ['id' => $post->id]) }}">削除</a>
+            <a href="{{ route('post.delete', ['id' => $post->id]) }}" onclick="return confirm('こちらの投稿を削除してもよろしいでしょうか？')">削除</a>
           </div>
+          @endif
         </div>
 
         <div class="contributor d-flex">
