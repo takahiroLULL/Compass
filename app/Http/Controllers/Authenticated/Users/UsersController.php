d1e23ba1997975d60be50dd7f8523940403bfa9c
高橋広昇 <>
2023-02-01 17:10:40 +0900

    検索機能の確認のため、プッシュ

--- app/Http/Controllers/Authenticated/Users/UsersController.php
+++ app/Http/Controllers/Authenticated/Users/UsersController.php
@@ -21,6 +21,7 @@ class UsersController extends Controller
         $gender = $request->sex;
         $role = $request->role;
         $subjects = $request->subject;// ここで検索時の科目を受け取る
+        // dd($subjects);
         $userFactory = new SearchResultFactories();
         $users = $userFactory->initializeUsers($keyword, $category, $updown, $gender, $role, $subjects);
         $subjects = Subjects::all();
